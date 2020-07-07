ae2d57024733e2f12a9921ca411157b35f09e2a3
HaJooHyun <mangchhe@naver.com>
2020-07-07 21:34:08 +0900

    JS: Project filtering2

--- main.js
+++ main.js
@@ -68,14 +68,19 @@ workBtnContainer.addEventListener("click", (e) => {
     if (filter == null) {
         return;
     }
-    projects.forEach((project) => {
-        console.log(project.dataset.type);
-        if (filter === "*" || filter === project.dataset.type) {
-            project.classList.remove("invisible");
-        } else {
-            project.classList.add("invisible");
-        }
-    });
+    projectContainer.classList.add("anim-out");
+
+    setTimeout(() => {
+        projectContainer.classList.remove("anim-out");
+        projects.forEach((project) => {
+            console.log(project.dataset.type);
+            if (filter === "*" || filter === project.dataset.type) {
+                project.classList.remove("invisible");
+            } else {
+                project.classList.add("invisible");
+            }
+        });
+    }, 300);
 
     console.log(filter);
 });
